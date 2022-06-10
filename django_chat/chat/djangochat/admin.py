12bc11cd929fe107caa0ee4c5adf6fcbd42cb95b
Ilya <67760549+leraks@users.noreply.github.com>
2022-06-10 00:33:15 -0700

    Delete django_chat directory

--- django_chat/chat/djangochat/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Room,Message,Dict_user
-# Register your models here.
-
-admin.site.register(Room)
-admin.site.register(Message)
-admin.site.register(Dict_user)
